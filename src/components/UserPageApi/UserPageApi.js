99e63640b16c2cbbedb638ef38e991de7bdf5846
Jason Phan <Phanx091@umn.edu>
2018-07-03 22:15:15 -0500

    clean code in userpageapi

--- src/components/UserPageApi/UserPageApi.js
+++ src/components/UserPageApi/UserPageApi.js
@@ -5,7 +5,6 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { API_ACTIONS } from "../../redux/actions/apiActions";
 
-
 // React-moment
 import Moment from "react-moment";
 
@@ -30,8 +29,7 @@ const styles = theme => ({
     margin: 0,
     overflow: "hidden",
     minWidth: 375,
-    transitionDuration: "0.3s",
-
+    transitionDuration: "0.3s"
   },
   media: {
     paddingTop: "56.25%" // 16:9
@@ -45,8 +43,7 @@ const styles = theme => ({
   },
   expandOpen: {
     transform: "rotate(180deg)"
-  },
-
+  }
 });
 
 const mapStateToProps = reduxState => ({
@@ -64,7 +61,6 @@ class UserPageApi extends Component {
 
   componentDidMount() {
     this.props.dispatch({ type: API_ACTIONS.FETCH_API });
-    this.defaultMesssage();
   }
   handleExpandClick = () => {
     this.setState({
@@ -74,57 +70,31 @@ class UserPageApi extends Component {
 
   render() {
     console.log("this.state.apiList:", this.state.apiList);
-
     const dataToFormat = (a, b) => {
       let dateA = new Date(a.pubDate);
       let dateB = new Date(b.pubDate);
       return dateB - dateA;
     };
-
-    this.defaultMesssage = () => {
-      const noArticlesArray = this.props.api.items; 
-      if (noArticlesArray.length === 0) {
-        return 'Click on the Menu button and add your first rss' 
-      }
-
-
-    }
-
-
-
-
-
-
-
     const { classes } = this.props;
     return (
       <div>
-     
-
         {this.props.api.items.sort(dataToFormat).map((data, i) => {
           return (
             <li key={i}>
               <Card className={classes.card}>
-                <CardHeader
-          
-                  title={data.title}
-             
-                />
+                <CardHeader title={data.title} />
                 <CardMedia
                   className={classes.media}
                   image={data.thumbnail}
                   title="something"
                 />
-
                 <CardContent>
                   {data.author}
                   <br />
                   <Moment fromNow>{data.pubDate}</Moment>
                 </CardContent>
                 <CardActions className={classes.actions} disableActionSpacing>
-          
                   <FavoriteIconButton favorite={data} />
-  
                   <IconButton aria-label="Share">
                     <SimpleModal link={data.link} />
                   </IconButton>
@@ -157,7 +127,6 @@ class UserPageApi extends Component {
             </li>
           );
         })}
-
       </div>
     );
   }
