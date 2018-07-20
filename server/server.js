dad23a40002acf724d6ac4234de62b090bb561fa
Jason Phan <Phanx091@umn.edu>
2018-07-20 17:09:31 -0500

    change port in server.js

--- server/server.js
+++ server/server.js
@@ -35,9 +35,9 @@ app.use('/api/fav', favRouter);
 app.use(express.static('build'));
 
 // App Set //
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 /** Listen * */
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
 });
